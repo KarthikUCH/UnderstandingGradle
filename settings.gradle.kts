488ee37cad5e8c4dd816322aaef1180b78ca584f
Raju Karthikeyan <rajuk@comfortdelgro.com>
2023-10-27 07:55:36 +0800

    Understanding the setting file

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+rootProject.name = "understanding-gradle"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+    }
+
+    // Reference To include additional Project to same Window: https://stackoverflow.com/a/29096428/2790197
+    includeBuild("../my-build-logic")
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+
+    // Reference : https://stackoverflow.com/a/29096428/2790197
+    includeBuild("../my-other-project")
+}
+
+include("app")
+include("business-logic")
+include("data-model")
